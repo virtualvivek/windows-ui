935dc0f27bfda946da3f3e348033b087bf98b8c7
Vivek Verma <virtualiz.me@gmail.com>
2020-08-12 00:10:50 +0530

    resolved missing modules

--- kitchen-sink/js/modules-demo/searchBarSuggestions_demo.js
+++
@@ -1,7 +0,0 @@
-//__init of view --
-//create_custom_searchAutoSuggestions(.class or #id, new selector name of your choice);
-
-$(document).ready(function () {
-    create_custom_searchAutos("#searchAutoBarNav","selectorid1");
-    create_custom_searchAutos(".searchAutoBar","selectorid2");
-});
