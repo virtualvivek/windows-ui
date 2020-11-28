0b80adbb83513e344172d9160948777c74490d1f
Vivek Verma <virtualiz.me@gmail.com>
2020-11-29 01:42:57 +0530

    updated for mobile responsive

---
+++ src/core/js/windows10framework.api.js
@@ -0,0 +1,13 @@
+
+// ----------------------------------------------- API setStatusBarAccent() -------------------------------------
+
+
+function setStatusBarAccent(){
+    var style = getComputedStyle(document.body);
+    var theColorIs = style.getPropertyValue('--AppColor');
+    
+    var meta=document.createElement('meta');
+    meta.name='theme-color';
+    meta.setAttribute('content', theColorIs);
+    document.getElementsByTagName('head')[0].appendChild(meta); 
+}
