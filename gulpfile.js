8b10ff5e2ca2834c0d696187a6b5b6b22e656659
Vivek Verma <virtualiz.me@gmail.com>
2023-01-05 03:14:37 +0530

    Improved Docs.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { src, dest } = require("gulp");
 const minifyJs = require("gulp-uglify");
 const concat = require("gulp-concat");
 const pug = require("gulp-pug");
+const formatHtml = require("gulp-format-html");
 
 const jsFiles = ["./src/js/api/**/*.js", "./src/js/components/**/*.js"]
 
@@ -13,8 +14,9 @@ const bundleJs = () => {
 }
 
 const compilePug = () => {
-  return src(['./docs/source/**/*.pug'])
+  return src(['./docs/source/**/*.pug', '!./docs/source/common/**/*.pug'])
     .pipe(pug({pretty: true}))
+    .pipe(formatHtml())
     .pipe(dest("./docs"));
 }
 
