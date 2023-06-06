d0bf1664d8bc2e53397567e3ed7819290f9f67c7
Vivek Verma <virtualiz.me@gmail.com>
2023-06-07 02:00:33 +0530

    Major UI Improvement

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -28,27 +28,28 @@ NavBarOverlay.addEventListener("click", toggleNavBar);
 
 
 // Scrolling Shadow ------------------------------------
-const NavBarWrap = document.querySelector(".app-navbar-wrap");
-const NavBarWrap_Nav = NavBarWrap.querySelector("nav");
-const Navbar_Header = NavBarWrap.querySelector(".app-navbar-header");
+const NavBar_ = document.querySelector(".app-navbar-wrap");
+const NavBar_Nav = NavBar_.querySelector("nav");
+const NavBar_List = document.getElementById("app-navbar-list");
+const Navbar_Header = NavBar_.querySelector(".app-navbar-header");
 
-NavBarWrap_Nav.addEventListener("scroll", (e) => {
+NavBar_List.addEventListener("scroll", (e) => {
   let _scrolTop_ = e.target.scrollTop;
   _scrolTop_ < 50
     ? Navbar_Header.style.boxShadow = "none"
-    : Navbar_Header.style.boxShadow = "0 6px 8px -8px var(--color-link-bg-active)";
+    : Navbar_Header.style.boxShadow = "0 4px 8px -8px #77777777";
 });
 
 
 // Resize Animation suppressor -----------------------
 function ResizedWin() {
-  NavBarWrap.style.transition = "";
-  NavBarWrap_Nav.style.transition = "";
+  NavBar_.style.transition = "";
+  NavBar_Nav.style.transition = "";
 }
 var on_resizew;
 window.onresize = function() {
   clearTimeout(on_resizew);
-  NavBarWrap.style.transition = "unset";
-  NavBarWrap_Nav.style.transition = "unset";
+  NavBar_.style.transition = "unset";
+  NavBar_Nav.style.transition = "unset";
   on_resizew = setTimeout(ResizedWin, 100);
 };
