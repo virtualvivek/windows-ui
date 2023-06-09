f38668a884e884b58d6fcb73b276566f35d85c4e
Vivek Verma <virtualiz.me@gmail.com>
2023-06-09 23:35:11 +0530

    Improved UI.

--- src/js/components/inputs.js
+++ src/js/components/inputs.js
@@ -7,3 +7,19 @@ for (const PwdToggler of PwdTogglers) {
     InputPass.type === "password" ? InputPass.type = "text" : InputPass.type = "password";
   });
 };
+
+
+const TxtClearers = document.querySelectorAll('[data-win-clear="text"]');
+
+for (const TxtClearer of TxtClearers) {
+ const getInputTxtID = TxtClearer.getAttribute("data-win-target");
+ const InputTxt = document.querySelector(getInputTxtID);
+ InputTxt.addEventListener("input", () => {
+  InputTxt.value !=="" ? TxtClearer.style.visibility = "visible" : TxtClearer.style.visibility = "hidden";
+});
+ TxtClearer.addEventListener("click", () => {
+    InputTxt.value = "";
+    InputTxt.focus();
+    TxtClearer.style.visibility = "hidden";
+  });
+};
